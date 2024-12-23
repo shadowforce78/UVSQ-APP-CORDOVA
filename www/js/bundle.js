dcb0f1396c740d73258f8a0ebeb05e9a5f099cf0
shadowforce78 <planque.adam@gmail.com>
2024-12-23 03:07:51 +0100

    Add Axios integration and build process; update .gitignore and HTML files

--- www/js/bundle.js
+++ www/js/bundle.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 // Supprimez la ligne d'import d'axios puisqu'il sera disponible globalement via CDN
 // const axios = window.axios;  // Optionnel: vous pouvez utiliser cette ligne si vous voulez être explicite
 
@@ -154,7 +155,6 @@ const connection = async (username, password) => {
             execution: token,
             _eventId: "submit",
             geolocation: "",
-            submit: "SE CONNECTER"
         });
 
         await axiosInstance.post(`${casUrl}?service=${serviceUrl}`, formData, {
@@ -184,3 +184,12 @@ module.exports = {
     connection,
     getAxiosInstance
 };
+},{}],2:[function(require,module,exports){
+const API = require('./API');
+
+// Expose les fonctions globalement
+window.edt = API.edt;
+window.connection = API.connection;
+window.getAxiosInstance = API.getAxiosInstance;
+
+},{"./API":1}]},{},[2]);
---
+++ www/js/webpackapi.js
@@ -0,0 +1,6 @@
+const API = require('./API');
+
+// Expose les fonctions globalement
+window.edt = API.edt;
+window.connection = API.connection;
+window.getAxiosInstance = API.getAxiosInstance;
